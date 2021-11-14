713adbe5b1eecdf1481e616c35395853931daea3
Pc <Pc@DESKTOP-BG78PBP>
2021-11-14 15:46:45 +0300

    Add project files.

---
+++ Faktöriyel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktöriyel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sonuc = 1;
+
+            Console.WriteLine("Bir sayı girin: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 1; i <= number; i++)
+            {
+                sonuc = sonuc * i;
+            }
+
+            Console.WriteLine("Girdiğiniz sayının faktöriyeli: " + sonuc);
+            Console.ReadLine();
+
+        }
+    }
+}
